88e27744350b5b9be6168d7a5fb61cdf6aca370a
Naman Sood <mail@nsood.in>
2020-08-28 03:27:43 -0400

    Add support for multiple image upload

--- README.md
+++ README.md
@@ -12,11 +12,13 @@ Make sure MongoDB is running on localhost and Node.js is installed.
 **API:**
 
 	POST /images/add
-		expects field "photo"
+		expects field "photos" file photo files
 		returns JSON object:
-			status: either "ok" or "err"
-			id: if status is ok then ID of image
-			error: if status is err then error message
+			files: array of objects indicating status of uploaded photo
+				filename: original uploaded filename (for reference, not preserved on server)
+				status: either "ok" or "err"
+				id: if status is ok then ID of image
+				error: if status is err then error message
 
 	GET /images/:id
 		returns photo with requested ID, or 404 if not found
--- index.js
+++ index.js
@@ -4,22 +4,27 @@ const multer = require('multer');
 const crypto = require('crypto');
 const fs = require('fs');
 
-
-
 const storage = multer.diskStorage({
 	destination: './images'
 });
+
 const upload = multer({
 	storage: storage,
 	fileFilter: function(req, file, callback) {
 		const supportedFiletypes = ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'];
 		if(supportedFiletypes.indexOf(file.mimetype) < 0) {
-			req.incorrectFileType = true;
-			return callback(null, false, new Error('Please upload a valid image!'));
+			if(!req.skippedFiles) {
+				req.skippedFiles = [ file.originalname ];
+			}
+			else {
+				req.skippedFiles.push(file.originalname);
+			}
+			return callback(null, false);
 		}
 		callback(null, true);
 	}
 });
+
 const app = express();
 
 const viewsDir = __dirname + '/views';
@@ -28,36 +33,46 @@ app.get('/', function(req, res) {
 	res.sendFile(viewsDir + '/index.html');
 });
 
-app.post('/images/add', upload.single('photo'), function(req, res) {
-	if(req.file) {
-		console.log('Received file ' + req.file.originalname);
+app.post('/images/add', upload.array('photos'), function(req, res) {
+	const response = {
+		files: []
+	}
+
+	for(var file of req.files) {
+		console.log('Received file ' + file.originalname);
 
 		const pic = new Photo;
 
 		const id = crypto.randomBytes(8).toString('hex');
 
 		pic.id = id;
-		pic.extn = req.file.mimetype.split('/')[1];
+		pic.extn = file.mimetype.split('/')[1];
 
 		fs.renameSync(
-			__dirname + '/images/' + req.file.filename,
+			__dirname + '/images/' + file.filename,
 			__dirname + '/images/' + pic.filename()
 		);
 
 		pic.save();
 
-		res.send({
+		response.files.push({
+			filename: file.originalname,
 			status: 'ok',
 			id: id
 		});
 	}
 
-	else {
-		res.send({
-			status: 'err',
-			error: req.incorrectFileType ? 'wrongfile' : 'nofile'
-		});
+	if(req.skippedFiles) {
+		for(var filename of req.skippedFiles) {
+			response.files.push({
+				filename: filename,
+				status: 'err',
+				err: 'badextn'
+			});
+		}
 	}
+
+	res.send(response);
 });
 
 app.get('/images/:id', function(req, res) {
@@ -80,8 +95,6 @@ app.get('/images/:id', function(req, res) {
 		});
 	});
 });
-
-
 
 app.listen(4242, function() {
 	console.log('Listening on port 4242');
